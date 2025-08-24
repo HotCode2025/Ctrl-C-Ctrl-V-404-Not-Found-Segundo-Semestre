4fd2b3155432fcaa4901ed865db4ae193e63c011
Rocio Lucero <rocioluceroal@gmail.com>
2025-08-23 23:16:57 -0300

    Delete Javascript/Clase 2 directory
    Error rama

--- Javascript/Clase 2/Es_array.js
+++
@@ -1,6 +0,0 @@
-const autos = ['Ferrari','Renault','BMW'];
-//Como preguntar si es una Array o Arreglo
-console.log(Array.isArray(autos)); //Devuelve un booleano
-
-//Segunda forma
-console.log(autos instanceof Array); //Preguntamos si la variable es una instancia de la clase Array
--- Javascript/Clase 2/Recorrido_elementos.js
+++
@@ -1,9 +0,0 @@
-const autos = ['Ferrari','Renault','BMW'];
-
-//Recorremos los elementos de un arreglo
-console.log(autos[0]);//recorro manual el primer elemento
-console.log(autos[2]);//recorro manual el último elemento
-//ciclo for
-for(let i = 0; i < autos.length; i++){
-    console.log(i+' : '+autos[i]);
-}
