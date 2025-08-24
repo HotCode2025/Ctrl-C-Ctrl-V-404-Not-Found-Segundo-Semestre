a8ef106d4fd7efbc6b77b992e99160ae967d4e2d
rocio2102 <rocioluceroal@gmail.com>
2025-08-23 22:42:32 -0300

    Carpeta JS

---
+++ Javascript/Clase 2/Es_array.js
@@ -0,0 +1,6 @@
+const autos = ['Ferrari','Renault','BMW'];
+//Como preguntar si es una Array o Arreglo
+console.log(Array.isArray(autos)); //Devuelve un booleano
+
+//Segunda forma
+console.log(autos instanceof Array); //Preguntamos si la variable es una instancia de la clase Array
