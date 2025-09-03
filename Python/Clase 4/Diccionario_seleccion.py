6831f063d53518dc00cad18cc971e134d3f078e3
rocio2102 <rocioluceroal@gmail.com>
2025-09-03 00:05:55 -0300

    Ejercicio diccionario (video)

---
+++ Python/Clase 4/Diccionario_seleccion.py
@@ -0,0 +1,11 @@
+#Diccionario seleccion argentina
+seleccionArgentina = {
+    10: {'Nombre': 'Lionel Messi', 'Edad': 35, 'Altura': 1.70, 'Precio': '50 Millones', 'Posición': 'Extremo derecho'},
+    11: {'Nombre': 'Angel Di Maria', 'Edad': 34, 'Altura': 1.80, 'Precio': '12 Millones', 'Posición': 'Extremo derecho'},
+    21: {'Nombre': 'Paulo Dybala', 'Edad': 28, 'Altura': 1.77, 'Precio': '35 Millones', 'Posición': 'Media punta'},
+    19: {'Nombre': 'Nicolas Otamendi', 'Edad': 34, 'Altura': 1.83, 'Precio': '3.5 Millones', 'Posición': 'Defensa'},
+    1: {'Nombre': 'Franco Armani', 'Edad': 35, 'Altura': 1.89, 'Precio': '3.5 Millones', 'Posición': 'Portero'}
+}
+
+for i in seleccionArgentina:
+    print(f'{i} -> {seleccionArgentina[i]}')
