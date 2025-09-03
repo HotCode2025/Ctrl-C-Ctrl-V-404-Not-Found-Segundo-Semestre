8c41fc84c0c121a58f780f2447eede66156e6ebb
rocio2102 <rocioluceroal@gmail.com>
2025-09-03 00:14:02 -0300

    Teoria funciones (video)

---
+++ Python/Clase 4/Funciones/Funciones_definicion.py
@@ -0,0 +1,6 @@
+#Definimos una funcion
+def mifuncion(): #Para identificar a la funcion utilizamos parentesis
+    print('Saludos a todos los alumnnos de la Tecnicatura')
+
+mifuncion() #Estamos llamando a la funcion
+mifuncion() #Podemos llamarla N cantidad de veces
