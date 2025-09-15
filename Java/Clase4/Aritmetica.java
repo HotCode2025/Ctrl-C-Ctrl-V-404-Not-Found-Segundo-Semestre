3405e5f14c94aec4172f81d738c9bc4023c07091
Delfina Mulinetti <delfinamulinetti@gmail.com>
2025-09-15 20:06:59 -0300

    Agrego ejercicios Clase4 (Aritmetica, Ciclos06, Ejercicio06, EjercicioCiclos06)

---
+++ Java/Clase4/Aritmetica.java
@@ -0,0 +1,11 @@
+package operaciones;
+
+public class Aritmetica {
+    int a;
+    int b;
+    
+    public void sumarNumeros() {
+     int resultado = a + b; 
+     System.out.println("resultado = " + resultado);
+    }
+}
---
+++ Java/Clase4/Ejercicio06.java
@@ -0,0 +1,20 @@
+package Ciclos06;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Ejercicio06 {
+      public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero, suma = 0;
+        do {
+            
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+            suma+= numero; 
+        }while(numero != 0);
+    JOptionPane.showMessageDialog(null, "La suma de todos los numeros ingresados" +suma);
+    }
+    
+    
+}
+
