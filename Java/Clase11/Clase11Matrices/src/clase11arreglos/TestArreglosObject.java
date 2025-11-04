69a5f7a32270a26e5a8faa122f2a0d0ef1b2c7e6
HugoZanatta95 <hugo-zanatta@hotmail.com>
2025-11-04 20:09:49 -0300

    Terminando Clase de Java

---
+++ Java/Clase11/Clase11Matrices/src/clase11arreglos/TestArreglosObject.java
@@ -0,0 +1,23 @@
+package clase11arreglos;
+
+import clase11arreglosDomain.Persona;
+
+public class TestArreglosObject {
+    public static void main(String[] args) {
+        Persona personas [] = new Persona[2];
+        personas[0] = new Persona("Hugo");
+        personas[1] = new Persona("Amara");
+        System.out.println("personas 0 = " + personas [0]);
+        System.out.println("personas 1 = " + personas [1]);
+        
+        for(int i = 0; i < personas.length; i ++){
+            System.out.println("personas "+i+" = " + personas[i]);
+        }
+        
+        //Sintaxis resumida
+        String frutas[] = {"Banana", "Kiwi", "Sandia"};
+        for (int i = 0; i < frutas.length; i++){
+            System.out.println("frutas " +i+" = "+ frutas[i]);
+        }
+    }    
+}
