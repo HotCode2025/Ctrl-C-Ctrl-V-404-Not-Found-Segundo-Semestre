5c48de9f496b6c3e1bd9161e4cb19f72f1aa2468
Santiagocapi <s.capitani@alumno.um.edu.ar>
2025-11-10 21:35:52 -0300

    Add Python class 10 (OOP Part 7)

---
+++ Python/Clase 10 (POO Parte 7 Diseño, Constante y contexto estático)/miClase.py
@@ -0,0 +1,47 @@
+class MiClase:
+    
+    variable_clase = 'Esta es una variable de clase'
+
+    def __init__(self, variable_instancia):
+        self.variable_instancia = variable_instancia
+        
+    # Metodo static, se asocia a la clase
+    @staticmethod
+    def metodo_estatico():
+        print(MiClase.variable_clase)
+     
+    # Metodo de clase    
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase)
+        
+    def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_clase)
+        print(self.variable_instancia)
+        
+print(MiClase.variable_clase)
+miClase1 = MiClase('Esta es una variable de instancia')
+print(miClase1.variable_instancia)
+print(miClase1.variable_clase)
+miClase2 = MiClase('Esta es otra prueba de variable de instancia')
+print(miClase2.variable_instancia)
+print(miClase2.variable_clase)
+
+# Creacion de variables de clase
+MiClase.variable_clase2 = "Valor de variable de clase 2"
+print(MiClase.variable_clase2)
+print(miClase1.variable_clase2)
+print(miClase2.variable_clase2)
+
+# Muestra el metodo estatico
+MiClase.metodo_estatico()
+
+# Muestra el metodo de clase
+MiClase.metodo_clase()
+
+miObjeto1 = MiClase('variable de instancia')
+miObjeto1.metodo_clase()
+miObjeto1.metodo_clase()
+miObjeto1.metodo_instancia()
---
+++ Python/Clase 10 (POO Parte 7 Diseño, Constante y contexto estático)/test_constante.py
@@ -0,0 +1,9 @@
+from constantes import MI_CONSTANTE
+from constantes import Matematicas as Mate
+
+print(MI_CONSTANTE)
+# Ahora vemos que si se puede modificar
+# MI_CONSTANTE = 'Aqui esta el nuevo valor de la constante'
+# print(MI_CONSTANTE)
+
+print(Mate)
